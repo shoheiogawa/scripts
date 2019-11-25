8c2fc2c50b88ef4b4811b55006cbda4be19d8114
Shohei Ogawa <shoheio@andrew.cmu.edu>
2019-11-24 20:34:27 -0500

    Update and rename the script
    No change in its functionality.

--- remove_trailing_space_backslash.sh
+++ remove_trailing_space_backslash.sh
@@ -1,7 +1,6 @@
 # How to use this:
-# $remove_tailing_space_backslash example.txt
+# $> remove_trailing_space_backslash example.txt
 
 sed -i '' 's/[[:space:]]\{1,\}$//' $@
-sed -i '' 's/[:\\]\{1,\}$//' $@
+sed -i '' 's/[:\\]\{1,\}$//'       $@
 sed -i '' 's/[[:space:]]\{1,\}$//' $@
-
